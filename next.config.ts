421a66961a510d82cfce9685e894ec6c7923a86b
Shanu S <shanu.s@surveysparrow.com>
2025-10-18 21:39:45 +0530

    whispr rewrite

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/whispr',
+  assetPrefix: '/whispr',
 };
 
 export default nextConfig;
