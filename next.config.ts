986aad6d2fc565eabc8135859525a5af3789f3c9
Shanu S <shanu.s@surveysparrow.com>
2025-12-08 20:17:13 +0530

    chore: update next.config.ts to include serverExternalPackages and remove keyv override from package.json and pnpm-lock.yaml

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: '/whispr',
   assetPrefix: '/whispr',
+  serverExternalPackages: ['ably', '@ably/chat'],
 };
 
 export default nextConfig;
